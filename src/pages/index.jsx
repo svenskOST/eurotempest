04015f513aba3f7df2742b29bb425df1627d2b86
Alexander Marini <svenskost6@gmail.com>
2025-07-24 02:50:34 +0200

    done with a fully working tailwind theme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,21 @@
 const Home = () => {
    return (
-      <div className='font-body bg-neutral-600 w-'>
-         <h1 className='text-primary-100 text-sm'>Hello World (text-xs)</h1>
-         <h1 className='text-primary-100 text-2x text-sm'>Hello World (text-sm)</h1>
-         <h1 className='text-primary-100 text-base'>Hello World (text-base)</h1>
-         <h1 className='text-primary-100 text-lg'>Hello World (text-lg)</h1>
-         <h1 className='text-primary-100 text-xl'>Hello World (text-xl)</h1>
-         <h1 className='text-primary-100 text-2xl'>Hello World (text-2xl)</h1>
-         <h1 className='text-primary-100 text-3xl'>Hello World (text-3xl)</h1>
-         <h1 className='text-primary-100 text-4xl font-display'>Hello World (text-4xl)</h1>
-         <h1 className='text-primary-100 text-5xl'>Hello World (text-5xl)</h1>
-         <h1 className='text-primary-900 text-6xl'>Hello World (text-6xl)</h1>
-         <h1 className='text-primary-100 text-7xl'>Hello World (text-7xl)</h1>
-         <h1 className='text-primary-100 text-8xl'>Hello World (text-8xl)</h1>
-         <h1 className='text-primary-100 text-9xl'>Hello World (text-9xl)</h1>
+      <div>
+         <h1 className='text-primary-300 text-xs'>Hello World (text-xs)</h1>
+         <h1 className='text-primary-300 text-sm'>Hello World (text-sm)</h1>
+         <h1 className='text-primary-300 text-base'>Hello World (text-base)</h1>
+         <h1 className='text-primary-300 text-lg'>Hello World (text-lg)</h1>
+         <h1 className='text-primary-300 text-xl'>Hello World (text-xl)</h1>
+         <h1 className='text-primary-300 text-2xl'>Hello World (text-2xl)</h1>
+         <h1 className='text-primary-300 text-3xl'>Hello World (text-3xl)</h1>
+         <h1 className='text-primary-300 text-4xl'>Hello World (text-4xl)</h1>
+         <h1 className='text-primary-300 text-5xl'>Hello World (text-5xl)</h1>
+         <h1 className='text-primary-300 text-6xl'>Hello World (text-6xl)</h1>
+         <h1 className='text-primary-300 text-7xl'>Hello World (text-7xl)</h1>
+         <h1 className='text-primary-300 text-8xl'>Hello World (text-8xl)</h1>
+         <h1 className='bg-primary-300 text-9xl raised-900 shadow-primary-200 top-10 absolute rounded-full p-6 hover:raised-100 transition-shadow'>
+            Hello World (text-9xl)
+         </h1>
       </div>
    )
 }
