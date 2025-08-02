f3ee8a3ddb6b7a3e16572cd71a1644aa526a2f06
Alexander Marini <svenskost6@gmail.com>
2025-08-02 23:35:57 +0200

    Added SEO

--- src/pages/credentials.jsx
+++ src/pages/credentials.jsx
@@ -4,7 +4,6 @@ export default function Credentials() {
    return (
       <>
          <div className='bg-neutral-100 min-h-screen flex items-center'>
-            <Image src='img1.jpg' width={1536} height={616} alt=''></Image>
          </div>
       </>
    )
