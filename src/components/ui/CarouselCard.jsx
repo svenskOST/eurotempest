cedf140f887ce97e34a06cbf1e8c0c154c6ac781
Alexander Marini <svenskost6@gmail.com>
2025-08-13 05:11:59 +0200

    Start using motion for scroll triggered animations

--- src/components/ui/CarouselCard.jsx
+++ src/components/ui/CarouselCard.jsx
@@ -1,10 +1,18 @@
+import { motion } from 'motion/react'
+
 export default function CarouselCard({ icon: Icon, title, description, button }) {
   return (
-    <div className='flex w-full max-w-sm md:max-w-[26rem] px-8 flex-col items-center bg-dark-700 bg-linear-65 from-primary-300/50 to-blue-950/50 text-light-200 rounded-lg gap-8 py-16 shadow-lg/30 hover:scale-105 hover:shadow-xl/30 transition-[box-shadow,_scale] md:max-lg:h-[31rem]'>
+    <motion.div
+      className='flex w-full max-w-sm md:max-w-[26rem] px-8 flex-col items-center bg-dark-700 bg-linear-65 from-primary-300/50 to-blue-950/50 text-light-200 rounded-lg gap-8 py-16 shadow-lg/30 hover:scale-105 hover:shadow-xl/30 transition-[box-shadow,_scale] md:max-lg:h-[31rem]'
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+    >
       <div className=''>{<Icon className='text-800 size-14' />}</div>
       <h3 className='weight-600 text-500 text-center'>{title}</h3>
-      <p className='text-300 flex-1 text-light-300'>{description}  </p>
+      <p className='text-300 flex-1 text-light-300'>{description} </p>
       <div className='md:w-4/5'>{button}</div>
-    </div>
+    </motion.div>
   )
 }
--- src/components/ui/SectionTitle.jsx
+++ src/components/ui/SectionTitle.jsx
@@ -1,3 +1,15 @@
+import { motion } from 'motion/react'
+
 export default function SectionTitle({ title }) {
-  return <h1 className='text-center text-700 weight-600 font-display text-dark-900 md:text-800 w-full'>{title}</h1>
+  return (
+    <motion.h1
+      className='text-center text-700 weight-600 font-display text-dark-900 md:text-800 w-full'
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+    >
+      {title}
+    </motion.h1>
+  )
 }
